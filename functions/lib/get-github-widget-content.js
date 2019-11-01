7f06bd2c126cc6439e2519c8cba17cf119458631
Chris Vogt <mail@chrisvogt.me>
2019-11-01 08:17:17 -0700

    update the widget query to select more data

--- functions/lib/get-github-widget-content.js
+++ functions/lib/get-github-widget-content.js
@@ -1,94 +1,28 @@
 const graphqlGot = require("graphql-got");
+const fs = require("fs");
+const path = require("path");
 
-const query = `
-query getGitHubWidgetContent($username: String!) {
-	user(login: $username) {
-    login
-    name
-		status {
-		  id
-      createdAt
-      message
-		}
-    projectsUrl
-    location
-    avatarUrl
-    pinnedRepositories(last: 10){
-			totalCount
-			nodes {
-				createdAt
-				description
-				descriptionHTML
-				hasIssuesEnabled
-				hasWikiEnabled
-				homepageUrl
-				licenseInfo {
-					name
-					nickname
-					key
-					url
-				}
-				name
-				nameWithOwner
-				openGraphImageUrl
-				primaryLanguage {
-					color
-					name
-				}
-				updatedAt
-				pushedAt
-				url
-				usesCustomOpenGraphImage
-			}
-		}
-    pullRequests(last: 1, states: MERGED) {
-      nodes {
-        labels(last: 10) {
-          edges {
-            node {
-              id
-              color
-              name
-            }
-          }
-        }
-        lastEditedAt
-        authorAssociation
-        url
-        closed
-        closedAt
-        createdAt
-        merged
-        number
-        repository {
-          name
-          id
-          url
-        }
-        title
-        url
-        viewerDidAuthor
-      }
-    }
-	}
-}`;
+const query = fs.readFileSync(
+  path.resolve(__dirname, "../queries/github-widget-content.gql"),
+  "utf8"
+);
 
 const getGitHubWidgetContent = async ({ context }) => {
-    const {
-        config: {
-          github: { access_token: token, username }
-        }
-      } = context;
-    
-      const { body } = await graphqlGot("https://api.github.com/graphql", {
-        query,
-        token,
-        variables: {
-          username
-        }
-      });
+  const {
+    config: {
+      github: { access_token: token, username }
+    }
+  } = context;
 
-      return body;
+  const { body } = await graphqlGot("https://api.github.com/graphql", {
+    query,
+    token,
+    variables: {
+      username
+    }
+  });
+
+  return body;
 };
 
 module.exports = getGitHubWidgetContent;
