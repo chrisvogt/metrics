ae109d26149ea51a5ef0c0a6eca08f41d3d791e1
Chris Vogt <mail@chrisvogt.me>
2020-02-09 18:50:52 -0800

    WIP: update to a common response schema

--- functions/lib/get-spotify-widget-content.js
+++ functions/lib/get-spotify-widget-content.js
@@ -1,17 +1,42 @@
 const getUserProfile = require('../api/spotify/get-user-profile');
 const getTopTracks = require('../api/spotify/get-top-tracks');
 
+const trackToCollectionItem = require('../transformers/trackToCollectionItem');
+
+// NOTE(cvogt): this widget content handler is returning what I'm testing as a
+// new common schema for widget content.
 const getSpotifyWidgetContent = async () => {
+  // NOTE(cvogt): these are mocked for development
+  const playlistsCount = 61;
+  const profile = await getUserProfile();
+  const topTracks = await getTopTracks();
 
-    // NOTE(cvogt): these are stubbed until a mechanism for getting and storing
-    // provider access tokens has been built
-    const profile = await getUserProfile();
-    const topTracks = await getTopTracks()
-
-    return {
-        profile,
-        topTracks
-    };
+  return {
+    collections: {
+      topTracks: topTracks.map(trackToCollectionItem)
+    },
+    metrics: [
+      {
+        displayName: 'Followers',
+        id: 'followers-count',
+        value: profile.followers.total
+      },
+      {
+        displayName: 'Playlists',
+        id: 'playlists-count',
+        value: playlistsCount
+      }
+    ],
+    provider: {
+      displayName: 'Spotify',
+      id: 'spotify'
+    },
+    profile: {
+      avatarURL: profile.images[0].url,
+      displayName: profile.display_name,
+      profileURL: profile.external_urls.spotify
+    }
+  };
 };
 
 module.exports = getSpotifyWidgetContent;
