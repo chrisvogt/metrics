8f6e8b9d20a0f1a357a4527068a7af39da264f5a
Chris Vogt <mail@chrisvogt.me>
2020-02-23 12:36:05 -0800

    feat: add job to sync top tracks daily

--- functions/api/spotify/get-top-tracks.js
+++ functions/api/spotify/get-top-tracks.js
@@ -1,7 +1,24 @@
-const { items } = require('./top-tracks.mock.json')
+const request = require('requestretry')
 
-const getTopTracks = async () => {
-  return items
+const getTopTracks = async accessToken => {
+  try {
+    const response = await request({
+      fullResponse: false,
+      headers: { Authorization: `Bearer ${accessToken}` },
+      json: true,
+      qs: {
+        time_range: 'short_term',
+        limit: 12
+      },
+      retryStrategy: err => !!err,
+      uri: 'https://api.spotify.com/v1/me/top/tracks'
+    })
+
+    return [response, null];
+  } catch (error) {
+    console.error('Error fetching top tracks from Spotify.');
+    return [null, error];
+  }
 }
 
 module.exports = getTopTracks
