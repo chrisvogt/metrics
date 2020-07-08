7d6703928e2d9dd6b17b01a3f124d18e58692f7e
Chris Vogt <mail@chrisvogt.me>
2020-07-08 01:01:22 -0700

    lint the files

---
+++ functions/lib/get-instagram-widget-content.js
@@ -0,0 +1,7 @@
+const instagramWidgetContentMock = require('./instagram-widget-content.mock.json')
+
+const getInstagramWidgetContent = async () => {
+  return instagramWidgetContentMock
+}
+
+module.exports = getInstagramWidgetContent
