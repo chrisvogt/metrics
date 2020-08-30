6392d1adccec62166435eb942c14dc605474266d
Chris Vogt <mail@chrisvogt.me>
2020-08-30 10:37:26 -0700

    feat: update Spotify job to store raw and transformed responses (#12)

--- functions/api/spotify/get-user-profile.js
+++ functions/api/spotify/get-user-profile.js
@@ -1,7 +1,15 @@
-const profile = require('./user-profile.mock.json')
+const request = require('requestretry')
 
-const getUserProfile = async () => {
-  return profile
+const getUserProfile = async accessToken => {
+  const response = await request({
+    fullResponse: false,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true,
+    retryStrategy: err => !!err,
+    uri: 'https://api.spotify.com/v1/me'
+  })
+  
+  return response
 }
 
 module.exports = getUserProfile
