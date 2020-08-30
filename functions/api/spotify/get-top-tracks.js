5788a033d99d47c3eca4dced0f36df7fe37e8180
Chris Vogt <mail@chrisvogt.me>
2020-08-30 10:37:26 -0700

    feat: update Spotify job to store raw and transformed responses (#12)

--- functions/api/spotify/get-top-tracks.js
+++ functions/api/spotify/get-top-tracks.js
@@ -1,24 +1,23 @@
 const request = require('requestretry')
 
 const getTopTracks = async accessToken => {
-  try {
-    const response = await request({
-      fullResponse: false,
-      headers: { Authorization: `Bearer ${accessToken}` },
-      json: true,
-      qs: {
-        time_range: 'short_term',
-        limit: 12
-      },
-      retryStrategy: err => !!err,
-      uri: 'https://api.spotify.com/v1/me/top/tracks'
-    })
+  const { items } = await request({
+    fullResponse: false,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true,
+    qs: {
+      time_range: 'short_term',
+      limit: 12
+    },
+    retryStrategy: err => !!err,
+    uri: 'https://api.spotify.com/v1/me/top/tracks'
+  })
 
-    return [response, null];
-  } catch (error) {
-    console.error('Error fetching top tracks from Spotify.');
-    return [null, error];
+  if (!items || !items.length > 0) {
+    throw new Error('No top tracks returned from Spotify.')
   }
+  
+  return items
 }
 
 module.exports = getTopTracks
