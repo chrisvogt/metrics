5af4b2fdcc585c93ab5f423f44de7664be5fdddd
Chris Vogt <mail@chrisvogt.me>
2020-08-30 17:18:07 -0700

    WIP: update all routes to disconnect the old context obj

--- functions/index.js
+++ functions/index.js
@@ -3,154 +3,62 @@ const cors = require('cors')({ origin: true })
 const express = require('express')
 const functions = require('firebase-functions')
 
+const { getWidgetContent, validWidgetIds } = require('./lib/get-widget-content')
 const syncInstagramData = require('./jobs/sync-instagram-data')
 const syncSpotifyTopTracks = require('./jobs/sync-spotify-top-tracks')
-
-const getGitHubWidgetContent = require('./lib/get-github-widget-content')
-const getGoodreadsWidgetContent = require('./lib/get-goodreads-widget-content')
-const getInstagramWidgetContent = require('./lib/get-instagram-widget-content')
-const getSpotifyWidgetContent = require('./lib/get-spotify-widget-content')
-
-const getPinnedRepositories = require('./get-pinned-repositories')
-const getWidgetContent = require('./get-widget-content')
 
 const firebaseServiceAccountToken = require('./token.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(firebaseServiceAccountToken),
-  databaseURL: 'https://personal-stats-chrisvogt.firebaseio.com'
+  databaseURL: 'https://personal-stats-chrisvogt.firebaseio.com',
 })
-
-// TODO(chrisvogt): factor out all uses of this "context" object – it's not needed
-const config = functions.config()
-const database = admin.firestore()
-const context = { config, database }
 
 exports.syncSptifyTopTracks = functions.pubsub
   .schedule('every day 02:00')
-  .onRun(() => syncSpotifyTopTracks(context));
+  .onRun(() => syncSpotifyTopTracks())
 
 exports.syncInstagramData = functions.pubsub
   .schedule('every day 02:00')
-  .onRun(() => syncInstagramData());
+  .onRun(() => syncInstagramData())
 
-exports.getPinnedRepositories = functions.https.onRequest(async (req, res) => {
-  return cors(req, res, async () => {
-    const repositories = await getPinnedRepositories(context)
-    res.set('Cache-Control', 'public, max-age=3600, s-maxage=14400')
-    res.set('Access-Control-Allow-Origin', '*')
-    res.status(200).send(repositories)
-  })
-})
-
-exports.getSummaries = functions.https.onRequest(async (req, res) => {
-  return cors(req, res, async () => {
-    const summariesRef = database.collection('summaries').doc('last_30_days')
-    const doc = await summariesRef.get()
-    const data = doc.data()
-    res.set('Cache-Control', 'public, max-age=3600, s-maxage=14400')
-    res.set('Access-Control-Allow-Origin', '*')
-    res.status(200).send(data)
-  })
-})
-
-exports.debugSyncSpotify = functions.https.onRequest(async (req, res) => {
-  return cors(req, res, async () => {
-    const response = await syncSpotifyTopTracks(context)
-    res.set('Cache-Control', 'public, max-age=3600, s-maxage=14400')
-    res.set('Access-Control-Allow-Origin', '*')
-    res.status(200).send(response)
-  })
-})
-
-
-const buildSuccessResponse = payload => ({
+const buildSuccessResponse = (payload) => ({
   ok: true,
-  payload
+  payload,
 })
 
 const buildFailureResponse = (err = {}) => ({
   ok: false,
-  error: err.message || err
+  error: err.message || err,
 })
-
-// NOTE(chrisvogt): the widgets API is the first set of endpoints to use Express
-// instead of direct Firebase routes.
 
 const app = express()
 
-app.get('/api/widgets', (req, res) => {
-  console.log('MULTI-WIDGET')
-  res.status(200).send('multi-widget endpoint')
-})
+app.get('/api/widgets/:provider', async (req, res) => {
+  const { params: { provider } = {} } = req
 
-app.get('/api/widgets/github', async (req, res) => {
+  if (!provider || !validWidgetIds.includes(provider)) {
+    const response = buildFailureResponse({
+      message: 'A valid provider type is required.',
+    })
+    res.status(404).send(response)
+    return res.end()
+  }
+
   try {
-    const githubWidgetContent = await getGitHubWidgetContent({ context })
+    const githubWidgetContent = await getWidgetContent(provider)
     const response = buildSuccessResponse(githubWidgetContent)
     res.status(200).send(response)
   } catch (err) {
     const response = buildFailureResponse(err)
     res.status(400).send(response)
   }
+
+  return res.end();
 })
 
-app.get('/api/widgets/goodreads', async (req, res) => {
-  try {
-    const goodreadsWidgetContent = await getGoodreadsWidgetContent({ context })
-    const response = buildSuccessResponse(goodreadsWidgetContent)
-    res.status(200).send(response)
-  } catch (err) {
-    const response = buildFailureResponse(err)
-    res.status(400).send(response)
-  }
-})
-
-app.get('/api/widgets/instagram', async (req, res) => {
-  try {
-    const instagramWidgetContent = await getInstagramWidgetContent()
-    const response = buildSuccessResponse(instagramWidgetContent)
-    res.status(200).send(response)
-  } catch (err) {
-    const response = buildFailureResponse(err)
-    res.status(400).send(response)
-  }
-})
-
-app.get('/api/widgets/spotify', async (req, res) => {
-  try {
-    const spotifyWidgetContent = await getSpotifyWidgetContent()
-    const response = buildSuccessResponse(spotifyWidgetContent)
-    res.status(200).send(response)
-  } catch (err) {
-    const response = buildFailureResponse(err)
-    res.status(400).send(response)
-  }
-
+app.get('*', (req, res) => {
+  res.status(404).send(404)
 })
 
 exports.app = functions.https.onRequest(app)
-
-// exports.widgetContentAPI = functions.https.onRequest(async (req, res) => {
-//   return cors(req, res, async () => {
-//     try {
-//       console.log(req.params);
-//       res.status(200).send('ok')
-//       return req.params
-//       const payload = await getSpotifyWidgetContent()
-//       res.set('Cache-Control', 'public, max-age=3600, s-maxage=14400')
-//       res.set('Access-Control-Allow-Origin', '*')
-//       res.status(200).send({
-//         ok: true,
-//         payload
-//       })
-//     } catch (error) {
-//       const { message } = error
-//       res.status(400).send({
-//         ok: false,
-//         error: { message }
-//       })
-//     }
-//   })
-// })
-
