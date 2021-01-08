65b8e1ded952fc81f34217741f6b54e9d1cfac17
Chris Vogt <mail@chrisvogt.me>
2021-01-07 23:26:05 -0800

    feat: fetch playlists from Spotify (#17)

--- functions/selectors/config.js
+++ functions/selectors/config.js
@@ -1,12 +1,46 @@
 const get = require('lodash/get')
 
 /**
- * Select Google Books API Key
- *
- * Selects the Google Books API key from the config.
+ * Select the Google Books API Key.
  */
 const selectGoogleBooksAPIKey = config => get(config, 'google.books_api_key')
 
+/**
+ * Select the Spotify Client ID.
+ * @see {@link https://developer.spotify.com/documentation/general/guides/authorization-guide/}
+ * @param {object} config The current app configuration. 
+ * @returns {string} The Spotify Client ID associated with your application.
+ */
+const selectSpotifyClientId = config => get(config, 'spotify.client_id')
+
+/**
+ * Select the Spotify Client Secret.
+ * @see {@link https://developer.spotify.com/documentation/general/guides/authorization-guide/}
+ * @param {object} config The current app configuration. 
+ * @returns {string} The Spotify Client Secret associated with your application.
+ */
+const selectSpotifyClientSecret = config => get(config, 'spotify.client_secret')
+
+/**
+ * Select the Spotify Redirect URI.
+ * @see {@link https://developer.spotify.com/documentation/general/guides/authorization-guide/}
+ * @param {object} config The current app configuration. 
+ * @returns {string} Must match value set in your Spotify developer dashboard.
+ */
+const selectSpotifyRedirectURI = config => get(config, 'spotify.redirect_uri')
+
+/**
+ * Select the Spotify Refresh Token.
+ * @see {@link https://developer.spotify.com/documentation/general/guides/authorization-guide/}
+ * @param {object} config The current app configuration. 
+ * @returns {string} The latest Spotify Refresh Token. Used in place of an auth code.
+ */
+const selectSpotifyRefreshToken = config => get(config, 'spotify.refresh_token')
+
 module.exports = {
-  selectGoogleBooksAPIKey
+  selectGoogleBooksAPIKey,
+  selectSpotifyClientId,
+  selectSpotifyClientSecret,
+  selectSpotifyRedirectURI,
+  selectSpotifyRefreshToken
 }
