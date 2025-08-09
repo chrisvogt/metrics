04fcb463c6c975faa228f13c4ba7fc509da43c12
Chris Vogt <mail@chrisvogt.me>
2025-08-08 20:18:36 -0700

    feat: add Firebase Auth user creation/deletion handlers

---
+++ functions/jobs/delete-user.js
@@ -0,0 +1,37 @@
+import admin from 'firebase-admin'
+import { logger } from 'firebase-functions'
+
+import { DATABASE_COLLECTION_USERS } from '../constants.js'
+
+const deleteUser = async (userRecord) => {
+  const { uid } = userRecord
+
+  const db = admin.firestore()
+
+  try {
+    await db
+      .collection(DATABASE_COLLECTION_USERS)
+      .doc(uid)
+      .delete()
+
+    logger.info('User deleted successfully from database.', {
+      uid
+    })
+
+    return {
+      result: 'SUCCESS'
+    }
+  } catch (error) {
+    logger.error('Failed to delete user from database.', {
+      uid,
+      error: error.message
+    })
+
+    return {
+      result: 'FAILURE',
+      error: error.message
+    }
+  }
+}
+
+export default deleteUser
